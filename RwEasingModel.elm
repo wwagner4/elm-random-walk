523e8c6c97ccfe89aaa4ebce3b2126ae0f8ee31f
wwagner4 <wwagner4@gmail.com>
2016-01-23 20:57:46 +0100

    added model

--- RwEasingModel.elm
+++ RwEasingModel.elm
@@ -2,10 +2,17 @@ import Time exposing (..)
 import Easing exposing (..)
 import Signal exposing (..)
 import Graphics.Element exposing (..)
+import Maybe exposing (..)
+import Graphics.Collage exposing (..)
+import Color exposing (..)
 
 
-showTime : Time -> Element
-showTime time = show time
+
+type alias Model =
+  { x : Float
+  , y : Float
+  , startTime : Time }
+
 
 
 timeSig : Signal Time
@@ -14,7 +21,24 @@ timeSig = foldp (\a b -> b + 10) 0 (Time.fps 10)
 
 anim : Time -> Float
 anim currentTime =
-    ease easeOutBack float 0 10 second currentTime
+    ease easeOutBack float 0 500 second currentTime
+
+
+showModel : Model -> Element
+showModel model =
+  let
+    shape = circle 20
+    form = shape
+      |> filled Color.red
+      |> alpha 0.4
+      |> move (model.x, model.y)
+  in
+    collage 500 500 [form]
+
+
+
+showTime : Time -> Element
+showTime time = show time
 
 
 showAnim : Time -> Element
